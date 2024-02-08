ce23e31b6cc37e5e4bcfa9e82325c7f499d70415
Dev_Lee <toxic023@naver.com>
2024-02-08 21:23:22 +0900

    binary prac

--- binary/binary_1.py
+++ binary/binary_1.py
@@ -3,21 +3,29 @@
 이진탐색
 반으로 쪼개면서 탐색하기
 """
-
-
 def binary_search(array, target, start, end):
-    if start > end:
-        return
-    mid = (start + end) // 2
-    if array[mid] == target:
-        return mid
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid - 1)
-    else:
-        return binary_search(array, target, mid + 1, end)
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return None
 
 
-n, target = list(map(int, input().split()))
+N = int(input())
 array = list(map(int, input().split()))
+array.sort()
 
-print(binary_search(array, target, 0, n - 1) + 1)
+M = int(input())
+target = list(map(int, input().split()))
+
+for i in target:
+    result = binary_search(array, i, 0, N - 1)
+    if result is not None:
+        print("YES", end=" ")
+    else:
+        print("NO", end=" ")
--- binary/binary_3.py
+++ binary/binary_3.py
@@ -7,24 +7,29 @@
 떡 갯수 = 4
 요구 떡 길이 = 6
 떡 19 15 10 17
+
+=============|          0
+=============|=======   6
+=========    |          0
+=============|===       3
+             |          9
 """
 
-N, K = map(int, input().split())
-array = list(map(int, input().split()))
+N, M = map(int, input().split())  # 떡 N, 요구 M
+array = list(map(int, input().split()))  # 떡 길이들
 
 start = 0
-end = max(array) - 1
+end = max(array)
 
 result = 0
 while start <= end:
     total = 0
     mid = (start + end) // 2
-
     for i in array:
         if i > mid:
             total += i - mid
 
-    if total < K:
+    if total < M: ## 최소 요구 떡보다 길이가 작으면 범위 설정
         end = mid - 1
     else:
         result = mid
